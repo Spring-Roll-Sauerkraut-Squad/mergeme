110aa41009eb5e50a780489f5ba327f23b73f97e
LukasK <11014902@stud.hochschule-heidelberg.de>
2024-04-28 15:53:30 +0200

    Altitude Control Adjustments

--- airplane-tracker-app/altitude-control/AltitudeControl.jsx
+++ airplane-tracker-app/altitude-control/AltitudeControl.jsx
@@ -1,17 +1,23 @@
 import "./AltitudeControl.css";
 import React from "react";
 import Map from "../airplane-collision/Map.jsx";
+import PlaceholderMap from "../placeholder/PlacerholderMap.jsx";
 
 const AltitudeControl = () => {
     return (
-        <div className="text-area">
-            <div>
-                <h1>Altitude Control</h1>
+        <div className="altitude-content">
+            <div className="description" >
+                <h2> Click on an airplane to get more information! </h2>
+            </div >
+            <div className="test">
+                <div className="altitude-details">
+                    <h1>Altitude Control</h1>
+                </div>
+                <div className="altitude-map">
+                    <PlaceholderMap />
+                </div>
             </div>
-            <div>
-                <Map />
-            </div>
-        </div>
+        </div >
     );
 }
 
