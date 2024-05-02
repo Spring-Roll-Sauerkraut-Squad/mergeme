a9e7a9b6e80aba8a2a8383b5ad25f7a127629513
LukasK <11014902@stud.hochschule-heidelberg.de>
2024-05-02 22:22:48 +0200

    Added Map Selector

--- airplane-tracker-app/altitude-control/AltitudeControl.jsx
+++ airplane-tracker-app/altitude-control/AltitudeControl.jsx
@@ -1,17 +1,19 @@
 import "./AltitudeControl.css";
 import React, { useState, useEffect } from "react";
 import FlightsDisplay from "./components/FlightsDisplay.jsx";
-import AltitudeMap from "./components/AltitudeMap.jsx";
+import LiveMap from "./components/LiveMap.jsx";
 import Filter from "./components/Filter.jsx";
 import Warning from "./components/Warning.jsx";
 
 import { FetchAirports } from "./scripts/FetchAirports.jsx";
 import { FetchFlights } from "./scripts/FetchFlights.jsx";
+import AltitudeMap from "./components/AltitudeMap.jsx";
 
 const AltitudeControl = () => {
     const [airports, setAirports] = useState([]);
     const [warning, setWarning] = useState(null);
     const [selectedFilter, setSelectedFilter] = useState('');
+    const [activeTab, setActiveTab] = useState('live-map');
 
     useEffect(() => {
         FetchAirports().then(data => {
@@ -36,18 +38,42 @@ const AltitudeControl = () => {
         <div className="altitude-window">
             <div className="info-description" >
                 <h2> Welcome to the Altitude Supervision! </h2>
-                <button onClick={handleTestWarning} style={{ margin: "10px", padding: "10px" }}>Test Warning</button>
+                <button className="warning-test-button" onClick={handleTestWarning}>Test Warning Message</button>
             </div >
             <div className="altitude-content">
-                <div className="flight-details">
+                <div className="details">
                     <h1> Active Flights: </h1>
                     <Filter setSelectedFilter={setSelectedFilter} />
                     <FlightsDisplay selectedFilter={selectedFilter} />
                 </div>
-                <div className="flight-map">
-                    <h1> | Live Map Data | </h1>
-                    <AltitudeMap airports={airports} />
-                </div>
+                <div className="details">
+
+                    <div className="tab-content">
+                        {activeTab === 'live-map' &&
+                            <div>
+                                <h1 className="map-title"> Live Data Map </h1>
+                                <LiveMap airports={airports} />
+                            </div>
+                        }
+                        {activeTab === 'altitude-map' &&
+                            <div>
+                                <h1 className="map-title"> Altitude Visualization Map </h1>
+                                <AltitudeMap />
+                            </div>
+                        }
+                        {activeTab === 'test-map' &&
+                            <div>
+                                <h1 className="map-title"> Test Map Data </h1>
+                                <LiveMap airports={airports} />
+                            </div>
+                        }
+                    </div>
+                    <div className="tab-selector">
+                        <button id="tab-button" style={{ borderRight: 'none' }} onClick={() => setActiveTab('live-map')} className={activeTab === 'live-map' ? 'active' : ''}>Live Map</button>
+                        <button id="tab-button" onClick={() => setActiveTab('altitude-map')} className={activeTab === 'altitude-map' ? 'active' : ''}>Altitude Map</button>
+                        <button id="tab-button" style={{ borderLeft: 'none' }} onClick={() => setActiveTab('test-map')} className={activeTab === 'test-map' ? 'active' : ''}>Test Map</button>
+                    </div>
+                </div >
             </div>
             {warning && <Warning message={warning} onClose={handleCloseWarning} />}
         </div >
---
+++ airplane-tracker-app/altitude-control/components/LiveMap.jsx
@@ -0,0 +1,39 @@
+import "./LiveMap.css";
+import React, { useEffect } from 'react';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+
+const LiveMap = ({ airports }) => {
+  useEffect(() => {
+    const map = L.map('live-map').setView([49.4, 8.7], 5);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+      maxZoom: 19,
+    }).addTo(map);
+
+    //Red Marker - Airports
+    const airportMarker = new L.Icon({
+      iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png',
+      shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+      iconSize: [25, 41],
+      iconAnchor: [12, 41],
+      popupAnchor: [1, -34],
+      shadowSize: [41, 41]
+    });
+
+    airports.forEach(airport => {
+      L.marker([airport.location[0].latitude, airport.location[0].longitude], { icon: airportMarker })
+        .addTo(map)
+        .bindPopup(`${airport.name}`);
+    });
+
+    return () => {
+      map.remove();
+    };
+  }, [airports]);
+
+  return <div id="live-map"></div>;
+};
+
+export default LiveMap;
