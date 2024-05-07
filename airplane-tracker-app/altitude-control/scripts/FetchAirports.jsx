e94963f21497fba6c7583d39ced3a7302ccb530c
LukasK <11014902@stud.hochschule-heidelberg.de>
2024-05-07 22:13:49 +0200

    Refactored Server setup into one single file

--- airplane-tracker-app/altitude-control/scripts/FetchAirports.jsx
+++ airplane-tracker-app/altitude-control/scripts/FetchAirports.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const FetchAirports = async () => {
     try {
-        const response = await axios.get('http://localhost:4000/api/airports');
+        const response = await axios.get('http://localhost:3000/api/airports');
         return response.data;
     } catch (error) {
         console.error('Error fetching airports:', error);
--- airplane-tracker-app/altitude-control/scripts/FetchAirspaces.jsx
+++ airplane-tracker-app/altitude-control/scripts/FetchAirspaces.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const FetchAirspaces = async () => {
     try {
-        const response = await axios.get('http://localhost:5000/api/airspaces');
+        const response = await axios.get('http://localhost:3000/api/airspaces');
         return response.data;
     } catch (error) {
         console.error('Error fetching airspaces:', error);
