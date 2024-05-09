294f1419182a08fea963db6d10c6f803512c5a1b
alex.exy <a.schyr@gmx.de>
2024-05-09 23:11:56 +0200

    added line draw

--- airplane-tracker-app/emergency-landing/components/MarkerMap.jsx
+++ airplane-tracker-app/emergency-landing/components/MarkerMap.jsx
@@ -7,6 +7,12 @@ import { fetchOpenSkyData } from '../api-connect/FetchOSAPI.js';
 const AirportsMap = ({ airports }) => {
   const [flights, setFlights] = useState([]);
 
+  // Function to create a line between two points
+  const createLine = (startCoords, endCoords, map) => {
+    const line = L.polyline([startCoords, endCoords], { color: 'blue' }).addTo(map);
+    return line;
+  };
+
   useEffect(() => {
     const map = L.map('marker-map').setView([49.4, 8.7], 5);
 
@@ -56,19 +62,47 @@ const AirportsMap = ({ airports }) => {
           const longitude = state[5];
           const latitude = state[6];
           const category = state[17];
+          let nearestAirport = null;
+          let nearestDistance = Infinity;
+
+          // Iterate through airports to find nearest
+          airports.forEach(airport => {
+            const airportLatitude = airport.latitude_deg;
+            const airportLongitude = airport.longitude_deg;
+            const distance = Math.sqrt(
+              Math.pow(latitude - airportLatitude, 2) +
+              Math.pow(longitude - airportLongitude, 2)
+            );
+
+            if (distance < nearestDistance) {
+              nearestAirport = airport;
+              nearestDistance = distance;
+            }
+          });
 
           // Check if longitude and latitude are available
-          if (longitude && latitude) {
+          if (longitude && latitude && nearestAirport) {
             // Create a green marker at the specified latitude and longitude
-            L.marker([latitude, longitude], { icon: airplaneMarker })
+            const airplane = L.marker([latitude, longitude], { icon: airplaneMarker })
               .addTo(map)
-              .bindPopup(`Flight: ${callsign}<br>Latitude: ${latitude}<br>Longitude: ${longitude}<br>Category: ${category}`);
+              .bindPopup(
+                `Flight: ${callsign}
+                <br>Latitude: ${latitude}
+                <br>Longitude: ${longitude}
+                <br>Category: ${category}
+                <br>Nearest Airport: ${nearestAirport.name}`
+              );
+
+            // Event listener to create a line when airplane marker is clicked
+            airplane.on('click', () => {
+              // Calculate coordinates of the nearest airport
+              const nearestAirportCoords = [nearestAirport.latitude_deg, nearestAirport.longitude_deg];
+
+              // Create a line between airplane and nearest airport
+              createLine([latitude, longitude], nearestAirportCoords, map);
+            });
+
           }
-
-          
-
-          
-
         });
 
       } catch (error) {
